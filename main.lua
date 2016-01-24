45607f8fb88c09e30505021d27a063dfafc38646
pilot114 <pilot114@bk.ru>
2016-01-24 17:28:53 +0000

    update animation

--- main.lua
+++ main.lua
@@ -1,10 +1,27 @@
 require "src/flirt"
 
 function love.load()
-	F.init()
-	-- TODO: to entityWrapper (for players and other character)
-	-- create and save in animation list
-	F.createAnim("playerRun", "player2", 64, 96, "1-6", 1, 0.1)
+	F.init(false)
+	
+	animations = {
+		-- TODO: fix both case
+		idle = F.createAnim("playerIdle", "player", 64, 96, "1-6:1", 0.1)
+		-- idle = F.createAnim("playerIdle", "cat", 512, 256, "1-2:1:1-2:2", 0.1)
+		-- walk<> run<> jump<> lookup<> stoop<> rise
+	}
+	spellRun = function(F)
+		love.graphics.print("hello", 300, 300)
+	end
+	controls = {
+		up = {"w", 4000},
+		left = {"a", 4000},
+		down = {"s", 4000},
+		right = {"d", 4000},
+		-- custom
+		spell = {"q", spellRun}
+	}
+	F.createPlayer(animations, controls, 100, 200)
+	-- F.createEnemy(aminations)
 end
 
 function love.update(dt)
@@ -15,7 +32,7 @@ function love.draw()
 	F.draw()
 end
 
--- or set F.debug = true in love.load()
+-- or set F.init(true)
 function love.keypressed(key)
 	if key == "`" then
 		F.debug = not F.debug
--- src/flirt.lua
+++ src/flirt.lua
@@ -1,13 +1,16 @@
 local anim8 = require "src/anim8"
 local sti = require "src/sti"
+local actors = require "src/actors"
 
 gr = love.graphics
 ph = love.physics
 kb = love.keyboard
+fs = love.filesystem
 
 F = {
 	version = "0.0.1",
 	debug = false,
+	-- only names (without preload)
 	assets = {
 		anim = {},
 		font = {},
@@ -16,65 +19,132 @@ F = {
 		music = {},
 		sound = {},
 	},
-	anim = {},
-	img = {},
+	-- obj cache for optimize load (one assets for many objects)
+	cache = {
+		anim = {},
+		font = {},
+		img = {},
+		music = {},
+		sound = {},
+	},
+
+	-- OBJECTS incapsulate assets, linked with them
+
+	-- (see createPlayer)
+	player = {},
 }
 
-function F.init()
+function F.createPlayer( animations, controls, x, y )
+	F.player = {
+		-- array {animObj, imageObj}
+		anim = animations,
+		-- ?
+		-- array {key, speed/callback, assets( anim/sound etc.)}
+		ctrl = controls,
+		x = x,
+		y = y,
+		dir = 1,
+	}
+	if F.player.anim.idle ~= nil then
+		F.player.animState = "idle"
+	end
+end
+
+-- function checkExistInCache(tableName, object)
+-- 	-- body
+-- end
+
+function F.init(debug)
+	F.debug = debug
 	F.registerAssets()
 end
 
 function F.registerAssets()
 	for k,v in pairs(F.assets) do
-		F.assets[k] = love.filesystem.getDirectoryItems( "assets/"..k )
+		F.assets[k] = fs.getDirectoryItems( "assets/"..k )
 	end
 end
 
-function F.createAnim( name, fname, x, y, frames, row, speed )
+function F.split( string, separator )
+	fields = {}
+	string:gsub("([^"..separator.."]*)"..separator, function(c) table.insert(fields, c) end)
+	return fields
+end
+
+function F.createAnim(aName, name, x, y, frames, speed )
 	for k,fileName in pairs(F.assets.anim) do
-	    for str in string.gmatch(fileName, "([^"..".".."]+)") do
-			if str == fname then
+	    for n,str in pairs(F.split(fileName, ".")) do
+			if str == name then
 				imageName = fileName
 				break
 			end
 	    end
 	end
-    i = gr.newImage("assets/anim/"..imageName)
-    g = anim8.newGrid(x, y, i:getWidth(), i:getHeight())
+	-- check exist imageObj
+	if F.cache.img[name] == nil then
+	    F.cache.img[name] = gr.newImage("assets/anim/"..imageName)
+	end
+	i = F.cache.img[name]
+	-- check exist animObj
+	if F.cache.anim[aName] == nil then
+		-- TODO: see https://github.com/kikito/anim8
+	    g = anim8.newGrid(x, y, i:getWidth(), i:getHeight())
+	    args = F.split(frames, ":")
+	    F.cache.anim[aName] = anim8.newAnimation(g(unpack(args)), speed)
+	end
+	a = F.cache.anim[aName]
 
-    F.img[name] = i
-    F.anim[name] = anim8.newAnimation(g(frames, row), speed)
+    return { a, i }
 end
 
 function F.update(dt)
-	for k,v in pairs(F.anim) do
-		v:update(dt)
+	if F.player.animState ~= nil then
+		curAnim = F.player.anim[F.player.animState]
+		curAnim[1]:update(dt)
 	end
 end
 
 function F.draw()
+	if F.player.animState ~= nil then
+		curAnim = F.player.anim[F.player.animState]
+		curAnim[1]:draw(curAnim[2], F.player.x, F.player.y)
+	end
+
 	if F.debug then
-		local py = 15
-		local px = 13
-		love.graphics.print("Flirt "..F.version, px, py)
+		gr.print("Flirt "..F.version, 10, 10)
 
-		F.assetsPrint( "Animations: ", F.assets.anim, py, px, 1 )
-		F.assetsPrint( "Images: ", 	   F.assets.img, py, px, 2 )
-		F.assetsPrint( "Fonts: ", 	   F.assets.font, py, px, 3 )
-		F.assetsPrint( "Music: ", 	   F.assets.music, py, px, 4 )
-		F.assetsPrint( "Sound: ", 	   F.assets.sound, py, px, 5 )
-	end
-
-	for k,v in pairs(F.anim) do
-		-- dirt
-		v:draw(F.img[k], 200, 200)
-	end
-end
-
-function F.assetsPrint( text, table, py, px, column )
-	love.graphics.print(text..#table, column*px*10, py*1)
-	for k,v in pairs(table) do
-		love.graphics.print(v, column*px*10, py*(k+1))
+		drawBlock = F.drawBlock("Assets: ", 10, 30)
+		drawBlock( "Animations: ", F.assets.anim)
+		drawBlock( "Images: ", 	 F.assets.img)
+		drawBlock( "Fonts: ", 	 F.assets.font)
+		drawBlock( "Music: ", 	 F.assets.music)
+		drawBlock( "Sound: ", 	 F.assets.sound)
+		
+		drawBlock = F.drawBlock("Cache: ", 10, 130)
+		-- TODO: what? empty cache?
+		drawBlock( "Animations: ", F.cache.anim)
+		drawBlock( "Images: ", 	 F.cache.img)
+		drawBlock( "Fonts: ", 	 F.cache.font)
+		drawBlock( "Music: ", 	 F.cache.music)
+		drawBlock( "Sound: ", 	 F.cache.sound)
 	end
 end
 
+function F.drawBlock(blockTitle, px, py, block)
+	gr.print(blockTitle, px, py)
+	local px = px
+	local py = py
+	local block = 1
+	local blockWidth = 130
+	local blockHeight = 12
+	function iter(text, table)
+		if #table > 0 then
+			gr.print(text..#table, px + block*blockWidth, py)
+			for k,v in pairs(table) do
+				gr.print(v, px + block*blockWidth, py + (k+1)*blockHeight)
+			end
+			block = block + 1
+		end
+	end
+	return iter
+end
